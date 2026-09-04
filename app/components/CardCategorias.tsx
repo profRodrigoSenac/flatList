7ed304c9fc8da5e88da8fa1b331443948d18b04d
Rodrigo Gomes dos Santos <rodrigo.santos@neomind.com.br>
2026-09-04 11:52:05 -0300

    começar os hooks

--- app/components/CardCategorias.tsx
+++ app/components/CardCategorias.tsx
@@ -1,45 +1,45 @@
-import {View, Image, FlatList, StyleSheet } from "react-native"
+import { View, Text, Image, FlatList, StyleSheet } from "react-native";
 
 
-export default function renderCategoria({item}:{item:any}){
-  return(
+export default function renderCategoria({ item }: { item: any }) {
+  return (
     <View style={styles.categorias}>
-    {item.titulo}
-    <FlatList
-      data={item.filmes}
-      keyExtractor={filme=>filme.id}
-      horizontal={true}
-      // showsHorizontalScrollIndicator={false}
-      contentContainerStyle={{paddingBottom:40}}
-      renderItem={({item})=>(
+      <Text style={styles.filmeTitulo}>{item.titulo}</Text>
+      <FlatList
+        data={item.filmes}
+        keyExtractor={filme => filme.id}
+        horizontal={true}
+        // showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 40 }}
+        renderItem={({ item }) => (
           <Image
-          source={{uri:item.imagem}}
-          style={[styles.filme]}
-          >             
+            source={{ uri: item.imagem }}
+            style={[styles.filme]}
+          >
           </Image>
-          
+
         )}
       > </FlatList>
-      
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-       categorias:{
-        color:"white",
-        backgroundColor: '#000000'
-       },
-       filme:{
-        width:100,
-        height:140,
-        borderRadius:8,
-        margin: 5,        
-        justifyContent:'flex-end',
-        padding:8
-       },
-       filmeTitulo:{
-        color:'white',
-        fontSize:12
-       }
+  categorias: {
+    color: "white",
+    backgroundColor: '#000000'
+  },
+  filme: {
+    width: 100,
+    height: 140,
+    borderRadius: 8,
+    margin: 5,
+    justifyContent: 'flex-end',
+    padding: 8
+  },
+  filmeTitulo: {
+    color: 'white',
+    fontSize: 12
+  }
 });
