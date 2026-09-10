ffe647d7c172afc7da69bec74cc6d1297d430f45
Rodrigo Gomes dos Santos <rodrigo.santos@neomind.com.br>
2026-09-10 14:39:36 -0300

    link

--- app/components/CardFilme.tsx
+++ app/components/CardFilme.tsx
@@ -3,7 +3,7 @@ import { Link } from "expo-router";
 
 export default function CardFilme({ item }: { item: any }) {
   return (
-    <Link href="/components/filme/{item.id}">
+    <Link href={`/components/filme/${item.id}`}>
       <Image
         source={{ uri: item.imagem }}
         style={styles.filme}
